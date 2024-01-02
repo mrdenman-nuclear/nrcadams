0fa0ce08a4fd8efb47ef99e6200914bcad6a2a32
Matthew Denman <71991832+mrdenman-nuclear@users.noreply.github.com>
2024-01-02 00:03:44 +0000

    Hermes!

--- R/simplify.R
+++ R/simplify.R
@@ -10,15 +10,16 @@ simplify_type = function(search_tbl, type_vct = Type) {
   search_tbl |>
     dplyr::mutate(
       {{type_vct}} := dplyr::case_when(
+        {{type_vct}} |> stringr::str_detect("Legal-Affidavit") ~ "Other",
+        {{type_vct}} |> stringr::str_detect("Legal") ~ "Legal",
         {{type_vct}} |> stringr::str_detect("FR Notice Comment Letter") ~ "Public Comments",
         {{type_vct}} |> stringr::str_detect("RAI") ~ "RAI",
+        {{type_vct}} |> stringr::str_detect("Audit") ~ "Audit",
         {{type_vct}} |> stringr::str_detect("SAR") ~ "Safety Analysis",
         {{type_vct}} |> stringr::str_detect("Safety Analysis") ~ "Safety Analysis",
         {{type_vct}} |> stringr::str_detect("SER") ~ "Safety Eval.",
         {{type_vct}} |> stringr::str_detect("Safety Evaluation") ~ "Safety Eval.",
-        {{type_vct}} |> stringr::str_detect("Audit") ~ "Audit",
         {{type_vct}} |> stringr::str_detect("Report") ~ "Report",
-        {{type_vct}} |> stringr::str_detect("Legal") ~ "Legal",
         {{type_vct}} |> stringr::str_detect("Slides") ~ "Slides",
         TRUE ~ "Other"
       )
