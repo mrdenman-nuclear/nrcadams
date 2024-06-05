4c031867748db1212885e1b7e554c25be5a07596
Matthew Denman <71991832+mrdenman-nuclear@users.noreply.github.com>
2024-06-05 23:49:28 +0000

    add inspection RSS search

--- R/ACRS.R
+++ R/ACRS.R
@@ -9,34 +9,11 @@
 search_ACRS <- function(
   days_back = 7
   ) {
-    start_date = Sys.Date() |> lubridate::ymd() - days_back
-
-    url = paste0(
-      nrcadams:::adams_search_head,
-      "properties_search_all:!(!(AuthorAffiliation,starts,NRC/ACRS,''),",
-      nrcadams:::adams_start_or_end(start_date, FALSE),
-      ")",
-      nrcadams:::adams_search_tail(FALSE)
+    nrcadams::search_advanced(
+      search_field = "properties_search_all:!(!(AuthorAffiliation,starts,NRC/ACRS,''),",
+      results_tag = "ACRS",
+      days_back = days_back
     )
-
-    paste("Searching with the following URL:\n", url,"\n") |>
-      tictoc::tic()
-    results = nrcadams:::make_results_tibble_no_docket(url, "ACRS")
-    tictoc::toc()
-
-    message(
-      paste(
-        "\n This search returned:", results |> dplyr::distinct()|> nrow(), "files."
-        )
-      )
-
-    if(results |> nrow() == 0) {
-      warning("\nThe search return no results.\n")
-    }
-    if(results |> nrow() >= 1000) {
-      warning("\nThis search returned more than 1000 results and thus may be incomplete. As a result, the search should be refined.\n")
-    }
-    return(results)
 }
 
 
@@ -90,3 +67,55 @@ make_results_tibble_no_docket = function(adams_url, tag_chr = NULL, download = F
     dplyr::arrange(dplyr::desc(`Publish Date`)) |>
     suppressWarnings()
 }
+
+#' Conduct ADAMS Search on ACRS Documents
+#'
+#' @param search_field chr: string with the search query
+#' @param results_tag chr: string with the results tag
+#' @param days_back dbl: Length of time the search extends in days since the document was published on ADAMS? Default is all time (i.e, NA). Cannot be used with start_date or end_date.
+#' 
+#' @source \url{https://www.nrc.gov/site-help/developers/wba-api-developer-guide.pdf}
+#' @return tibble of search results
+#' @export
+#'
+search_advanced <- function(
+  search_field = "properties_search_all:!(!(AuthorAffiliation,starts,NRC/ACRS,''),",
+  results_tag = "ACRS",
+  days_back = 7
+  ) {
+    start_date = Sys.Date() |> lubridate::ymd() - days_back
+
+    url = paste0(
+      nrcadams:::adams_search_head,
+      search_field,
+      nrcadams:::adams_start_or_end(start_date, FALSE),
+      ")",
+      nrcadams:::adams_search_tail(FALSE)
+    )
+
+    paste("Searching with the following URL:\n", url,"\n") |>
+      tictoc::tic()
+    results = nrcadams:::make_results_tibble_no_docket(url, results_tag)
+    tictoc::toc()
+
+    message(
+      paste(
+        "\n This search returned:", results |> dplyr::distinct()|> nrow(), "files."
+        )
+      )
+
+    if(results |> nrow() == 0) {
+      warning("\nThe search return no results.\n")
+    }
+    if(results |> nrow() >= 1000) {
+      warning("\nThis search returned more than 1000 results and thus may be incomplete. As a result, the search should be refined.\n")
+    }
+    return(results)
+}
+
+
+nrcadams::search_advanced(
+    search_field = "properties_search_all:!(!(DocumentType,eq,%27Inspection+Manual%27,%27%27),!(AuthorAffiliation,starts,NRC/NRR,%27%27),",
+    results_tag = "Inspections",
+    days_back = 7
+)
