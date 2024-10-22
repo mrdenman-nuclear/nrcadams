2e36adfed5b961c6f4e0e6fbd87092efe671960c
Matthew Denman <71991832+mrdenman-nuclear@users.noreply.github.com>
2024-10-22 16:53:46 +0000

    update for source term code reports

--- R/ACRS.R
+++ R/ACRS.R
@@ -73,6 +73,7 @@ make_results_tibble_no_docket = function(adams_url, tag_chr = NULL, download = F
 #' @param search_field chr: string with the search query
 #' @param results_tag chr: string with the results tag
 #' @param days_back dbl: Length of time the search extends in days since the document was published on ADAMS? Default is all time (i.e, NA). Cannot be used with start_date or end_date.
+#' @param content_search lgl: Is this a content search or a tag search
 #' 
 #' @source \url{https://www.nrc.gov/site-help/developers/wba-api-developer-guide.pdf}
 #' @return tibble of search results
@@ -81,17 +82,30 @@ make_results_tibble_no_docket = function(adams_url, tag_chr = NULL, download = F
 search_advanced <- function(
   search_field = "properties_search_all:!(!(AuthorAffiliation,starts,NRC/ACRS,''),",
   results_tag = "ACRS",
-  days_back = 7
+  days_back = 7,
+  content_search = FALSE
   ) {
     start_date = Sys.Date() |> lubridate::ymd() - days_back
 
-    url = paste0(
-      nrcadams:::adams_search_head,
-      search_field,
-      nrcadams:::adams_start_or_end(start_date, FALSE),
-      ")",
-      nrcadams:::adams_search_tail(FALSE)
-    )
+    if (content_search) {
+      url = paste0(
+        nrcadams:::adams_search_head,
+        "properties_search:!(",
+        nrcadams:::adams_start_or_end(start_date, FALSE),
+        "),",
+        search_field,
+        nrcadams:::adams_search_tail(content_search)
+      )
+    } else {
+      url = paste0(
+        nrcadams:::adams_search_head,
+        search_field,
+        nrcadams:::adams_start_or_end(start_date, FALSE),
+        ")",
+        nrcadams:::adams_search_tail(content_search)
+      )
+    }
+
 
     paste("Searching with the following URL:\n", url,"\n") |>
       tictoc::tic()
